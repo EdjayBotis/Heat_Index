6020f63a95d441fd194f2ead3a6bc46903d02686
Edjay <137255416+EdjayBotis@users.noreply.github.com>
2025-05-09 20:23:30 +0800

    Add files via upload

---
+++ heatIndexPic.h
@@ -0,0 +1,10 @@
+#ifndef HEAT_INDEX_PIC_H
+#define HEAT_INDEX_PIC_H
+
+#include <pgmspace.h>
+
+extern const unsigned char heatIndexPic[];
+
+extern const char* daysOfWeek[7];
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,133 +1,141 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_ILI9341.h>
 #include <RTClib.h>
 #include <DHT.h>
+#include "heatIndexPic.h"
 
 #define DHTPIN 4
-#define SWITCH_PIN 15
-#define BUZZER_PIN 18
+#define BUZZER_PIN 13
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+#define LED_RED 2
+#define LED_ORANGE 5
+#define LED_YELLOW 19
 
-const unsigned char theIcon [] PROGMEM = {
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 
-	0x00, 0x03, 0x00, 0x00, 0x01, 0x80, 0x18, 0x00, 0x03, 0xe0, 0x10, 0x00, 0x04, 0x27, 0x80, 0x00, 
-	0x04, 0x34, 0xc0, 0x00, 0x04, 0x30, 0x60, 0x00, 0x04, 0x30, 0x66, 0x00, 0x04, 0x30, 0x60, 0x00, 
-	0x04, 0x37, 0xc0, 0x00, 0x05, 0xb7, 0x80, 0x00, 0x05, 0xb0, 0x18, 0x00, 0x05, 0xb1, 0x08, 0x40, 
-	0x05, 0xb3, 0x00, 0xc0, 0x05, 0xb3, 0x00, 0xe0, 0x05, 0xb0, 0x01, 0xf0, 0x05, 0xb0, 0x03, 0xf0, 
-	0x05, 0xb0, 0x03, 0xf8, 0x0d, 0x98, 0x07, 0xfc, 0x1b, 0xe8, 0x06, 0xec, 0x17, 0xec, 0x0e, 0xde, 
-	0x17, 0xf4, 0x0f, 0xbe, 0x17, 0xe4, 0x0f, 0x7e, 0x13, 0xec, 0x0e, 0x4e, 0x19, 0xc8, 0x07, 0xfc, 
-	0x0c, 0x30, 0x07, 0xf8, 0x03, 0xe0, 0x01, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
+#define TFT_CS    14
+#define TFT_RST   26
+#define TFT_DC    27
+#define TFT_MOSI  23
+#define TFT_CLK   18
 
-Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 DHT dht(DHTPIN, DHT22);
-RTC_DS1307 rtc;
-
-void displayData();
-void displayOff();
-bool checkDanger(float temp, float hum);
+RTC_DS3231 rtc;
 
 void setup() {
-    pinMode(SWITCH_PIN, INPUT_PULLUP);
     pinMode(BUZZER_PIN, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_ORANGE, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+
     Serial.begin(115200);
-
-    if (!rtc.begin()) {
-        Serial.println("Couldn't find RTC");
-        while (1);
-    }
-
+    rtc.begin();
     dht.begin();
+    tft.begin();
+    
+    tft.fillScreen(ILI9341_BLACK);
+    tft.setRotation(1);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.setTextSize(3);
+    tft.setCursor(0, 50);
+    tft.println("HEAT INDEX: ");
 
-    if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-        Serial.println("SSD1306 allocation failed");
-        while (1);
-    }
+    tft.setTextColor(ILI9341_BLUE);
+    tft.setTextSize(2);
+    tft.setCursor(0, 185);
+    tft.print("Hum:");
 
-    oled.clearDisplay();
-    oled.setCursor(10, 7);
-    oled.setTextSize(2);
-    oled.setTextColor(WHITE);
-    oled.print("BSABE 3M2");
+    tft.setTextColor(ILI9341_RED);
+    tft.setTextSize(2);
+    tft.setCursor(150, 185);
+    tft.print("Temp:");
+    
+    tft.setTextColor(ILI9341_WHITE);
+    tft.setTextSize(2);
+    tft.setCursor(0, 140);
+    tft.println("Degree Celcius");
 
-    oled.setTextSize(1);
-    oled.setCursor(10, 40);
-    oled.print("By Edjay Botis");
-    oled.display();
-    delay(3000);
+    tft.setTextSize(1);
+    tft.setCursor(90, 230);
+    tft.drawLine(0,225,320,225,ILI9341_WHITE);
+    tft.println("BY BSABE 3M2: GROUP 2");
+
+    tft.drawBitmap(170,35,heatIndexPic,150,150,ILI9341_WHITE);
+
 }
 
 void loop() {
-    if (digitalRead(SWITCH_PIN) == LOW) {
-        displayData();
+        float temp = dht.readTemperature();
+        float hum = dht.readHumidity();
+        float heatIndex = dht.computeHeatIndex(temp, hum, false);
+        DateTime now = rtc.now();
+        uint8_t day = now.dayOfTheWeek();
+
+        tft.setTextColor(ILI9341_WHITE,ILI9341_BLACK);
+        tft.setTextSize(2);
+        tft.setCursor(0, 10);
+        tft.print(now.hour());
+        tft.print(":");
+        tft.print(now.minute());
+        tft.print(":");
+        tft.print(now.second());
+
+        tft.setCursor(130, 10);
+        tft.print(daysOfWeek[day]);
+
+        tft.setCursor(210, 10);
+        tft.print(now.year());
+        tft.print("/");
+        tft.print(now.month());
+        tft.print("/");
+        tft.print(now.day());
+
+        tft.setTextColor(ILI9341_WHITE,ILI9341_BLACK);
+        tft.setTextSize(6);
+        tft.setCursor(0, 90);
+        tft.println(heatIndex, 1);
+
+        tft.setTextSize(3);
+        tft.setTextColor(ILI9341_WHITE,ILI9341_BLACK);
+        tft.setCursor(50, 180);
+        tft.print(hum, 1);
+        tft.print("%");
+
+        tft.setTextColor(ILI9341_WHITE,ILI9341_BLACK);
+        tft.setTextSize(3);
+        tft.setCursor(220, 180);
+        tft.print(temp, 1);
+        tft.print("C");
+        
+    if (heatIndex >= 52) {
+        tone(BUZZER_PIN, 500);
+        delay(1000);
+        noTone(BUZZER_PIN);
+        delay(1000); 
+        digitalWrite(LED_RED, HIGH);
+        digitalWrite(LED_ORANGE, LOW);
+        digitalWrite(LED_YELLOW, LOW);
+
+    } else if (heatIndex >= 42) {
+        tone(BUZZER_PIN, 400);
+        delay(700);
+        noTone(BUZZER_PIN);
+        delay(700); 
+        digitalWrite(LED_RED, LOW);
+        digitalWrite(LED_ORANGE, HIGH);
+        digitalWrite(LED_YELLOW, LOW);
+    
+    } else if (heatIndex >= 33){
+        noTone(BUZZER_PIN);
+        digitalWrite(LED_RED, LOW);
+        digitalWrite(LED_ORANGE, LOW);
+        digitalWrite(LED_YELLOW, HIGH);
+        
     } else {
-        digitalWrite(BUZZER_PIN, LOW);
-        displayOff();
+        noTone(BUZZER_PIN);
+        digitalWrite(LED_RED, LOW);
+        digitalWrite(LED_ORANGE, LOW);
+        digitalWrite(LED_YELLOW, LOW);
+  
     }
-    delay(2000);
-}
-
-void displayData() {
-    float temp = dht.readTemperature();
-    float hum = dht.readHumidity();
-    float heatIndex = dht.computeHeatIndex(temp, hum, false);
-    DateTime now = rtc.now();
-
-    oled.clearDisplay();
-    oled.setTextSize(1);
-    oled.setTextColor(WHITE);
-
-    oled.setCursor(5, 0);
-    oled.print(now.year());
-    oled.print("-");
-    oled.print(now.month());
-    oled.print("-");
-    oled.print(now.day());
-
-    oled.setCursor(80, 0);
-    oled.print(now.hour());
-    oled.print(":");
-    oled.print(now.minute());
-    oled.print(":");
-    oled.print(now.second());
-
-    // Heat index
-    oled.setCursor(0, 15);
-    oled.print("Heat Index:");
-    oled.setTextSize(2);
-    oled.setCursor(10, 25);
-    oled.print(heatIndex, 1);
-    oled.drawCircle(65,25,2,WHITE);
-    oled.println(" C");
-
-    oled.setTextSize(1);
-    oled.setCursor(0, 50);
-    oled.print("Temp:");
-    oled.print(temp, 1);
-    oled.println("C");
-    oled.setCursor(70, 50);
-    oled.print("Hum:");
-    oled.print(hum, 1);
-    oled.println("%");
-
-    oled.drawBitmap(90, 12, theIcon, 32, 32, WHITE);
-
-    oled.display();
-
-    checkDanger(temp, hum);
-
-}
-
-bool checkDanger(float temp, float hum) {
-    bool danger = (temp > 40 || temp < 0 || hum < 20 || hum > 90);
-    digitalWrite(BUZZER_PIN, danger ? HIGH : LOW);
-    return danger;
-}
-
-void displayOff() {
-    oled.clearDisplay();
-    oled.display();
 }
